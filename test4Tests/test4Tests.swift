e8283b5c5721b84276eb6c47f2e8c3b415256a30
TrevorLee <trevor.lee@ichef.com.tw>
2017-04-11 20:33:37 +0800

    add print 1234

--- test4Tests/test4Tests.swift
+++ test4Tests/test4Tests.swift
@@ -28,9 +28,13 @@ class test4Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func test2(){
+        print("1234")
     }
     
 }
